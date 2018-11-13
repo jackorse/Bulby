fa6c634c19b8a41da19ebf47b89bcfb3cb2b2402
jack96489 <giacomo.orsenigo@gmail.com>
2018-11-13 19:17:39 +0100

    test github

--- Bulby/Bulby.ino
+++ Bulby/Bulby.ino
@@ -33,7 +33,6 @@ finestra* frame;// = finestra();
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
-	//test
 	Serial.begin(9600);
 	Serial.println("START");
 	delay(10);
