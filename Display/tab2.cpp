76f5c278f95878aa32b7139681459e9394507057
Giacomo Orsenigo <giacomo.orsenigo@gmail.com>
2018-11-25 18:50:54 +0100

    bulby

--- Display/tab2.cpp
+++ Display/tab2.cpp
@@ -11,7 +11,7 @@ tab2::tab2(Adafruit_GFX * gfx, TouchScreen * ts, CColore* colore)
 {
 	this->colore = colore;
 	piu = new button(gfx, 200, 100, 50, 50, WHITE, BLACK, "+", ts);
-	meno = NULL;//button(gfx, 100, 100, 50, 50, WHITE, BLACK, "-", ts);
+	meno = new button(gfx, 100, 100, 50, 50, WHITE, BLACK, "-", ts);
 	tabs[1]->setDisabled();
 }
 
@@ -30,9 +30,8 @@ void tab2::draw()
 	gfx->println("INTENSITA'");
 	//gfx->println("\n  altre impostazioni.....");
 	//gfx->println("\n  Coming soon...");
-	printIntensita();
 	piu->drawButton();
-	//meno->drawButton();
+	meno->drawButton();
 }
 
 int tab2::checkBottoni()
@@ -44,13 +43,13 @@ int tab2::checkBottoni()
 		printIntensita();
 		return PIU_BUTTON;
 	}
-	//else if (meno->checkTouch())
-	//{
-	//	restartTimer();
-	//	//colore->setIntensita(colore->getIntensita() -1);
-	//	//printIntensita();
-	//	//return MENO_BUTTON;
-	//}
+	else if (meno->checkTouch())
+	{
+		restartTimer();
+		colore->setIntensita(colore->getIntensita() - 1);
+		printIntensita();
+		return MENO_BUTTON;
+	}
 	return GenericTab::checkBottoni();
 }
 
