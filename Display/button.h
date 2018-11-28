11143cae401efe104d6ac47e9397881dc9389d03
jack96489 <giacomo.orsenigo@gmail.com>
2018-11-28 15:24:06 +0100

    RGBColors

--- Display/button.h
+++ Display/button.h
@@ -1,15 +1,7 @@
 #pragma once
 #include"Adafruit_GFX.h"
 #include"TouchScreen.h"
-// Assign human-readable names to some common 16-bit color values:
-#define	BLACK   0x0000
-#define	BLUE    0x001F
-#define	RED     0xF800
-#define	GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#include"RGBColors.h"
 
 
 #define TS_MINX 122
